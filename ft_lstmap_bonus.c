f744e16aeae44550a7f50996acc5465fc4401a30
aschmitt 42 <aschmitt@z1r1p5.42lyon.fr>
2023-11-15 16:14:15 +0100

    fin

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -6,33 +6,12 @@
 /*   By: aschmitt <aschmitt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/06 18:52:01 by aschmitt          #+#    #+#             */
-/*   Updated: 2023/11/15 15:43:26 by aschmitt         ###   ########.fr       */
+/*   Updated: 2023/11/15 16:05:45 by aschmitt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-/*
-t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
-{
-	t_list	*res;
-	t_list	*temp;
 
-	res = malloc(sizeof(t_list));
-	if (!res || !lst || !(*f) || !(*del))
-		return (NULL);
-	res->content = lst->content;
-	res->next = NULL;
-	temp = res;
-	while (lst != NULL && lst->next != NULL)
-	{
-		if (!temp)
-			return (NULL);
-		temp->next = ft_lstnew((*f)(lst->next->content));
-		lst = lst->next;
-	}
-	return (res);
-}
-*/
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new_list;
